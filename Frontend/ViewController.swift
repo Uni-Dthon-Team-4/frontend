c5fe32a0b43d0750782c21168fd57326079cb7cf
dev_jaeho <33758013+jaeho0718@users.noreply.github.com>
2024-11-02 10:56:35 +0900

    Initial Commit

---
+++ Frontend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Frontend
+//
+//  Created by Jaeho Lee on 11/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
